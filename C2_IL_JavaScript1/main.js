6f77f7ea8e98e3ad158d9c3b9410cf31865e3d25
hoy080 <78216714+hoy080@users.noreply.github.com>
2021-03-12 12:13:35 +0900

    Add files via upload

---
+++ C2_IL_JavaScript1/main.js
@@ -0,0 +1,77 @@
+var name = 'Yuk Hei';
+console.log(name);
+
+var myFavNumber = 19;
+console.log(myFavNumber);
+
+var myFavNumber = '19';
+console.log(myFavNumber);
+
+var hello = ('true');
+console.log(hello);
+
+var nameLetters = (['y','u','k','h','e','i'])
+console.log(nameLetters);
+
+var threeData =(['Tall','John','3 + 4'])
+console.log(threeData);
+
+var firstName = 'Yuk Hei';
+var lastName = 'Ho';
+var firstLast = firstName.concat(lastName);
+console.log(firstLast);
+
+var oneHundred = 32+68;
+console.log(oneHundred);
+
+console.log(nameLetters.length);
+
+var longWord = ('supercalifragilisticexpialidocious');
+console.log(longWord.length);
+
+var firstNameOne = 'Yuk Hei';
+var lastNameOne = 'Ho';
+if(firstNameOne.length == lastNameOne.length);
+console.log(firstNameOne.length === lastNameOne.length);
+
+var time = new Date() .getHours();
+if (time < 7){
+   message = 'Between 7 and 9:40';
+} else if (time < 9){
+   message = 'Not between 7 and 9:40';
+}
+console.log(time);
+let currentDate = new Date();
+if ( currentDate.getDay() == 0) {
+  console.log('Sunday');
+  }
+if ( currentDate.getDay() == 1) {
+  console.log('Monday');
+  }
+if ( currentDate.getDay() == 2) {
+  console.log('Tuesday');
+  }
+if ( currentDate.getDay() == 3) {
+  console.log('Wednesday');
+  }
+if ( currentDate.getDay() == 4) {
+  console.log('Thursday');
+  }
+if ( currentDate.getDay() == 5) {
+  console.log('Friday');
+  }
+if ( currentDate.getDay() == 6) {
+  console.log('Saturday');
+}
+
+var threeRestaurants =(['Katsu Midori', 'Afuri', 'MOS Burger']);
+console.log(threeRestaurants);
+
+var pandemic =(['Travel', 'Not wear a mask', 'Party']);
+console.log(pandemic);
+
+var newYorkCity =(['Home', 'Washington Square Park', 'China Town']);
+console.log(newYorkCity);
+
+var movies =(['The Secret of Walter Mitty', 'Moneyball', 'The Perks of Being a Wallflower']);
+console.log(movies);
